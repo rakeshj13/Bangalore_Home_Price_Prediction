c52a80a3f78c7aa797dc283ba31bd973f51318c3
RAKESH JADHAV <rakeshjadhav1307@yahoo.com>
2024-11-27 15:18:02 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import util  # Import the util.py module
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/get_location_names', methods=['GET'])
