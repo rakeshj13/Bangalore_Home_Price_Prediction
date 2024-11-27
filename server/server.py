5b4441d535a36ff02b012f49dcaad2daf9554134
RAKESH JADHAV <rakeshjadhav1307@yahoo.com>
2024-11-27 13:55:43 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -38,5 +38,10 @@ def predict_home_price():
 
     return response
 
+if __name__ == "__main__":
+    print("Starting Python Flask Server for Home Price Prediction...")
+    util.load_saved_artifacts()  # Load saved artifacts before running the server
+    app.run()
+
 
 
